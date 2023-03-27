926798687c4c9857e1dfd7aebe7c0c0d56a1852e
miłosz kaszubowski <milosz.kaszubowski@onet.pl>
2023-03-27 13:24:16 +0200

    NEW

--- Projekt2.cpp
+++ Projekt2.cpp
@@ -1,7 +1,67 @@
-﻿#include <iostream>
+﻿#include<iostream>
+#include<vector>
+#include<stack>
 using namespace std;
+
+struct graf {
+    bool stos = 0; //czy dodany na stos
+    vector <int> polaczenia; //do przechowywania połączeń
+    // dodatkowe opcje
+}*w;
+
+void odwiedz(int n)
+{
+    //wykonaj jakies czynnosci
+    //w przypadku odwiedzenia wierzcholka o numerze n
+    cout << "Odwiedzono wierzcholek o numerze: " << n << endl;
+}
+
+void DFS(int n)
+{
+    stack<int> stos;    //utworzenie stosu 
+    stos.push(n);  //dodanie pierwszego wierzcholka na stos
+    while (!stos.empty()) //dopóki jest cos na stosie
+    {
+        n = stos.top(); //pobranie elementu ze stosu
+
+        stos.pop(); //usuń pobrany element ze stosu
+        odwiedz(n); //odwiedź go i zrób coś
+        //oraz dodaj wszystkie jego nieodwiedzone i nie będące 
+        //na stosie połączenia na stos
+        for (int i = 0; i < w[n].polaczenia.size(); i++)
+            if (!w[w[n].polaczenia[i]].stos)
+            {
+                if (w[n].polaczenia[i] != 1)
+                {
+                    stos.push(w[n].polaczenia[i]);
+                    w[w[n].polaczenia[i]].stos = 1; //oznaczenie, że dodano na stos
+                    cout << "Dodano na stos wierzcholek nr " << w[n].polaczenia[i] << endl;
+                }
+            }
+    }
+}
 
 int main()
 {
-    cout << "Hello World!\n";
+    cout << "Podaj liczbe wierzcholkow w grafie: ";
+    int n, p, a, b;
+    cin >> n;
+    w = new graf[n + 1];//przydzielenie pamięci na wierzchołki grafu
+    //wczytanie wierzchołków grafu
+    cout << "Podaj liczbe polaczen: ";
+    cin >> p;
+
+    for (int i = 0; i < p; i++)
+    {
+        cout << "Podaj numery wierzcholkow, ktore chcesz ze soba polaczyc: ";
+        cin >> a >> b;
+        w[a].polaczenia.push_back(b); //połączenie jest dwukierunkowe a-->b
+        w[b].polaczenia.push_back(a); //b-->a
+    }
+    //przeszukaj graf
+    DFS(1); //rozpoczynamy od wierzchołka o numerze 1
+
+
+    delete[] w;
+    return 0;
 }
